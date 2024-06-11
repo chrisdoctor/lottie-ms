29af6c2a754d39f2f112fd6ac0a5441187df2bd4
Christopher Doctor <chris.doctor@tigerspike.com>
2024-06-11 18:32:28 +0800

    Integrate the preview in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import LottiePreview from './components/LottiePreview';
 
 const App: React.FC = () => {
   return (
     <div>
       <h1>Lottie MS</h1>
+      <LottiePreview />
     </div>
   );
 };
