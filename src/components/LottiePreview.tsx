0b67b7809c0f264435739343b1b16e5957b23c1c
Christopher Doctor <chris.doctor@tigerspike.com>
2024-06-12 18:55:12 +0800

    Add sized div around Lottie player

--- src/components/LottiePreview.tsx
+++ src/components/LottiePreview.tsx
@@ -3,7 +3,11 @@ import Lottie from "lottie-react";
 import lottie1 from "../data/lottie1.json";
 
 const LottiePreview: React.FC = () => {
-  return <Lottie animationData={lottie1} loop={true} />;
+  return (
+    <div style={{ height: "200px", width: "200px" }}>
+      <Lottie animationData={lottie1} loop={true} />
+    </div>
+  );
 };
 
 export default LottiePreview;
