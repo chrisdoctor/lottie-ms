611aa2187740e4d69c92a8b33cb878ecc8b114c1
Christopher Doctor <chris.doctor@tigerspike.com>
2024-06-15 14:40:20 +0800

    Refactor lottie preview component to fix issues with pointer events when in mobile form factor

--- src/components/LottiePreview.tsx
+++ src/components/LottiePreview.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import Lottie from "lottie-react";
 import lottie1 from "../data/lottie1.json";
 
 const LottiePreview: React.FC = () => {
+  const lottieRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const lottieElement = lottieRef.current;
+
+    if (lottieElement) {
+      lottieElement.style.pointerEvents = "none";
+    }
+  }, []);
+
   return (
-    <div style={{ height: "200px", width: "200px" }}>
-      <Lottie animationData={lottie1} loop={true} />
+    <div
+      ref={lottieRef}
+      className="border border-gray-300 rounded-lg flex justify-center items-center pointer-events-none"
+    >
+      <div className="h-40 w-40 items-center justify-center">
+        <Lottie className="relative z-10" animationData={lottie1} loop={true} />
+      </div>
     </div>
   );
 };
