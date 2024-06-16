5009257c359f0355d414896844b1f9311a4d8886
Christopher Doctor <chris.doctor@tigerspike.com>
2024-06-17 00:01:48 +0800

    Refactor tsconfig and webpack to implement offline mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+dotenv.config();
 
+console.log("PUBLIC_URL:", process.env.PUBLIC_URL);
+console.log("ENVS:", process.env);
 module.exports = {
   entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    publicPath: process.env.PUBLIC_URL || "/",
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"],
@@ -29,7 +37,35 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: "./public/index.html",
+      inject: "body",
+      templateParameters: {
+        PUBLIC_URL: process.env.PUBLIC_URL || "/",
+      },
+    }),
+    new WorkboxWebpackPlugin.InjectManifest({
+      swSrc: "./src/service-worker.ts",
+      swDest: "service-worker.js",
+      maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // Limit to 5 MB
+    }),
+    // new WorkboxWebpackPlugin.GenerateSW({
+    //   clientsClaim: true,
+    //   skipWaiting: true,
+    //   maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // Limit to 5 mb
+    // }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "public",
+          to: "",
+          globOptions: {
+            ignore: ["**/index.html"], // Ignore index.html
+          },
+        },
+      ],
+    }),
+    new webpack.DefinePlugin({
+      "process.env.PUBLIC_URL": JSON.stringify(process.env.PUBLIC_URL),
     }),
   ],
   devServer: {
