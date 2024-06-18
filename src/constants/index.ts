7600e939c0f41ad792e88e3d48e07d688a4e552e
chrisdoctor <chris.doctor@gmail.com>
2024-06-18 18:10:02 +0800

    Add api status constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,5 @@
 export const LOCALSTORAGE_UPLOADED_ITEMS_KEY = "offlineDataUploads";
 export const LOCALSTORAGE_CACHED_ITEMS_KEY = "offlineData";
+export const API_STATUS_LOADING = "loading";
+export const API_STATUS_SUCCESS = "success";
+export const API_STATUS_FAIL = "fail";
